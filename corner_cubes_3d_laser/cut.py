c77ea9385533d2d6f360be4b94c7d725a94f5fba
Ankur Gupta <rukna1000@gmail.com>
2017-02-27 09:49:35 -0500

    Finshed top edge for the uncut cube side C

--- corner_cubes_3d_laser/cut.py
+++ corner_cubes_3d_laser/cut.py
@@ -230,6 +230,7 @@ class Cut:
             if i % 2 == 0:
                 shape.add(self.__gen_line(last_pos, numpy.array([0, self.tab_width])))
                 last_pos += numpy.array([0, self.tab_width])
+
             else:
                 shape.add(self.__gen_line(last_pos, numpy.array([-Config.material_thickness, 0])))
                 last_pos += numpy.array([Config.material_thickness, 0])
@@ -243,7 +244,25 @@ class Cut:
         return shape
 
     def __gen_cut_c(self, starting_pos):
-        pass
+        shape = self.drawing.g(id=str(self.id))
+
+        # Top Edge
+        last_pos = starting_pos + numpy.array([self.tab_width, 0])
+        for i in xrange(self.tab_count - 2):
+            if i % 2 == 0:
+                shape.add(self.__gen_line(last_pos, numpy.array([0, Config.material_thickness])))
+                last_pos += numpy.array([0, Config.material_thickness])
+
+                shape.add(self.__gen_line(last_pos, numpy.array([self.tab_width, 0])))
+                last_pos += numpy.array([self.tab_width, 0])
+
+                shape.add(self.__gen_line(last_pos, numpy.array([0, -Config.material_thickness])))
+                last_pos += numpy.array([0, -Config.material_thickness])
+
+            else:
+                shape.add(self.__gen_line(last_pos, numpy.array([self.tab_width, 0])))
+                last_pos += numpy.array([self.tab_width, 0])
+
 
     def __gen_cut_a90(self, starting_pos):
         pass
