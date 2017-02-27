5f696ca3c9f75c6c64d5a0317eb264749636ae61
Ankur Gupta <rukna1000@gmail.com>
2017-02-27 10:36:26 -0500

    Added code for the bottom right corner

--- corner_cubes_3d_laser/cut.py
+++ corner_cubes_3d_laser/cut.py
@@ -338,6 +338,14 @@ class Cut:
 
         shape.add(self.__gen_line(last_pos, numpy.array([self.tab_width - Config.material_thickness, 0])))
 
+        # Bottom right corner
+        last_pos = starting_pos + numpy.array([self.length - Config.material_thickness, self.length - self.tab_width])
+
+        shape.add(self.__gen_line(last_pos, numpy.array([0, self.tab_width])))
+        last_pos += numpy.array([0, self.tab_width])
+
+        shape.add(self.__gen_line(last_pos, numpy.array([-(self.tab_width - Config.material_thickness), 0])))
+
 
     def __gen_cut_a90(self, starting_pos):
         pass
